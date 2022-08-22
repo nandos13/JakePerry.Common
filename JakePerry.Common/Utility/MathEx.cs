0e18174cd987dcd6a2aa69898d006b25b9c6735a
Jake Perry <jake-perry@live.com>
2022-08-22 13:16:29 +0930

    Added PosMod helper method

---
+++ JakePerry.Common/Utility/MathEx.cs
@@ -0,0 +1,20 @@
+﻿namespace JakePerry
+{
+    public static class MathEx
+    {
+        /// <summary>
+        /// Custom modulo operation that ensures a positive value is returned.
+        /// </summary>
+        public static int PosMod(int x, int m)
+        {
+            x %= m;
+            return x < 0 ? x + m : x;
+        }
+
+        /// <inheritdoc cref="PosMod(int, int)"/>
+        public static void PosMod(ref int x, int m)
+        {
+            x = PosMod(x, m);
+        }
+    }
+}
