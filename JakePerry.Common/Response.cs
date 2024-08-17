6c5fec466c38259b955031ca11ba079a37f59146
Jake Perry <jake-perry@live.com>
2024-08-17 19:09:02 +0930

    Response structs
    Add Response object structs which express the success or failed state of some arbitrary process. The generic struct also allows a value to be passed back.
    Move AssignValueUtility into Utility folder

---
+++ JakePerry.Common/Response.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+using System.Threading;
+
+namespace JakePerry
+{
+    /// <summary>
+    /// A response object representing the result of some arbitrary request.
+    /// </summary>
+    public readonly struct Response
+    {
+        private readonly ExceptionDispatchInfo m_exception;
+        private readonly bool m_success;
+
+        internal ExceptionDispatchInfo ExceptionInfo => m_exception;
+
+        /// <summary>
+        /// The exception associated with the failure.
+        /// <para/>
+        /// A successful Response will never contain an exception.
+        /// </summary>
+        public Exception Exception => m_exception?.SourceException;
+
+        /// <summary>
+        /// Indicates whether the request succeeded.
+        /// </summary>
+        public bool Success => m_success;
+
+        private Response(bool success, ExceptionDispatchInfo ex)
+        {
+            m_exception = ex;
+            m_success = success;
+        }
+
+        internal Response(ExceptionDispatchInfo ex)
+        {
+            m_exception = ex;
+            m_success = false;
+        }
+
+        /// <summary>
+        /// Construct a failed response object containing an exception.
+        /// <para/>
+        /// Unthrown exceptions are supported.
+        /// If the exception has no stack trace (ie. it has not been thrown),
+        /// a stack trace will be generated in place.
+        /// </summary>
+        public Response(Exception ex)
+        {
+            if (ex is not null)
+            {
+                if (ex.StackTrace is null)
+                {
+                    try
+                    {
+                        throw ex;
+                    }
+                    catch (Exception) { }
+                }
+
+                m_exception = ExceptionDispatchInfo.Capture(ex);
+            }
+            else
+            {
+                m_exception = null;
+            }
+            m_success = false;
+        }
+
+        /// <summary>
+        /// Throw the exception contained in this response, if there is one.
+        /// </summary>
+        public void Throw()
+        {
+            m_exception?.Throw();
+        }
+
+        /// <summary>
+        /// Check if a <paramref name="token"/> is canceled and create a failure response
+        /// containing an <see cref="OperationCanceledException"/> if so.
+        /// </summary>
+        /// <returns>
+        /// Returns <see langword="true"/> if cancellation was requested and execution
+        /// should be aborted; Otherwise, returns <see langword="false"/>.
+        /// </returns>
+        public static bool CheckCanceled(CancellationToken token, out Response canceledResponse)
+        {
+            try
+            {
+                token.ThrowIfCancellationRequested();
+            }
+            catch (OperationCanceledException ex)
+            {
+                canceledResponse = new Response(ex);
+                return true;
+            }
+
+            canceledResponse = default;
+            return false;
+        }
+
+        /// <summary>
+        /// Construct a failed response object with no exception.
+        /// </summary>
+        public static Response Failed() => new Response(false, null);
+
+        /// <summary>
+        /// Construct a successful response object.
+        /// </summary>
+        public static Response Successful() => new Response(true, null);
+
+        public static implicit operator Response(bool value)
+        {
+            return value ? Successful() : Failed();
+        }
+
+        public static implicit operator Response(Exception ex)
+        {
+            return new Response(ex);
+        }
+    }
+}
---
+++ JakePerry.Common/Response`1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace JakePerry
+{
+    /// <summary>
+    /// A response object representing the result of some arbitrary request.
+    /// </summary>
+    public readonly struct Response<T>
+    {
+        private readonly Response m_inner;
+        private readonly T m_value;
+
+        /// <inheritdoc cref="Response.Exception"/>
+        public Exception Exception => m_inner.Exception;
+
+        /// <inheritdoc cref="Response.Success"/>
+        public bool Success => m_inner.Success;
+
+        /// <summary>
+        /// Typeless response with no value object.
+        /// </summary>
+        public Response Typeless => m_inner;
+
+        /// <summary>
+        /// The value associated with the successful response.
+        /// <para/>
+        /// A failed Response will always return <see langword="default"/>.
+        /// </summary>
+        public T Value => m_value;
+
+        private Response(Response inner, T value)
+        {
+            m_inner = inner;
+            m_value = value;
+        }
+
+        /// <summary>
+        /// Construct a successful response containing the given <paramref name="value"/>.
+        /// </summary>
+        public Response(T value)
+        {
+            m_inner = Response.Successful();
+            m_value = value;
+        }
+
+        /// <summary>
+        /// Cast the value associated with this response object using the
+        /// given <paramref name="selector"/> function.
+        /// </summary>
+        /// <returns>
+        /// If the current response was successful, returns a new
+        /// Response object containing the current value casted to
+        /// type <typeparamref name="V"/>;
+        /// Otherwise, returns a failed Response object and the
+        /// <paramref name="selector"/> function is not invoked.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"/>
+        public Response<V> Cast<V>(Func<T, V> selector)
+        {
+            _ = selector ?? throw new ArgumentNullException(nameof(selector));
+            if (m_inner.Success)
+            {
+                var v = selector.Invoke(m_value);
+                return new Response<V>(v);
+            }
+            else
+            {
+                return new Response<V>(Response.Failed(), default);
+            }
+        }
+
+        /// <inheritdoc cref="Response.Throw"/>
+        public void Throw()
+        {
+            m_inner.Throw();
+        }
+
+        /// <inheritdoc cref="Response.Failed"/>
+        public static Response<T> Failed()
+        {
+            return new Response<T>(Response.Failed(), default);
+        }
+
+        public static implicit operator Response(Response<T> typed)
+        {
+            return typed.Typeless;
+        }
+
+        public static implicit operator Response<T>(Response typeless)
+        {
+            return new Response<T>(typeless, default);
+        }
+
+        public static implicit operator Response<T>(Exception ex)
+        {
+            return (Response<T>)new Response(ex);
+        }
+    }
+}
