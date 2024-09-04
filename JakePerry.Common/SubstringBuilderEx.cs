814eb1c8ed18e10dff56d75ac6ac4ad1f71054d7
Jake Perry <jake-perry@live.com>
2024-09-04 22:18:30 +0930

    StringBuilder Substring extensions
    Added SubstringBuilderEx, containg optimized Append, AppendLine, Insert methods for Substring.

---
+++ JakePerry.Common/SubstringBuilderEx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace JakePerry
+{
+    public static class SubstringBuilderEx
+    {
+        public static StringBuilder Append(this StringBuilder stringBuilder, Substring substring)
+        {
+            _ = stringBuilder ?? throw new ArgumentNullException(nameof(stringBuilder));
+            return stringBuilder.Append(substring.AsSpan());
+        }
+
+        public static StringBuilder AppendLine(this StringBuilder stringBuilder, Substring substring)
+        {
+            _ = stringBuilder ?? throw new ArgumentNullException(nameof(stringBuilder));
+            return stringBuilder.Append(substring.AsSpan()).AppendLine();
+        }
+
+        public static StringBuilder Insert(this StringBuilder stringBuilder, int index, Substring substring)
+        {
+            _ = stringBuilder ?? throw new ArgumentNullException(nameof(stringBuilder));
+            return stringBuilder.Insert(index, substring.AsSpan());
+        }
+    }
+}
